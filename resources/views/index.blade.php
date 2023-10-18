4786f4e1f52d4d9c88ebc1007da9a927ead0beeb
Thiago Tenório <thitccp@gmail.com>
2023-10-17 21:20:36 -0300

    21: blade templates

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<div>
+  Hello Im blade template!
+</div>
+
+@isset($name)
+  <div>
+    The name is: {{ $name }}
+  </div>
+@endisset
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Main page';
+    return view('index', [
+        'name' => 'Thiago'
+    ]);
 });
 
 Route::get('/hello', function() {
