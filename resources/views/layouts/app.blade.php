ee0416f470e3779c0d4de408017106c5de415276
Thiago Tenório <thitccp@gmail.com>
2023-10-31 13:52:21 -0300

    23: layouts using template inheritance

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <title> Laravel 10 Task List App </title>
+  </head>
+
+  <body>
+    <h1>@yield('title')</h1>
+    <div>
+      @yield('content')
+    </div>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\HTTP\Response;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -66,15 +67,25 @@ $tasks = [
   ),
 ];
 
+Route::get('/', function () {
+  return redirect()->route('tasks.index');
+});
 
-Route::get('/', function () use($tasks) {
-    return view('index', [
-        'tasks' => $tasks
-    ]);
-})->name('tasks.index ');
 
-Route::get('/{id}', function ($id) {
-    return 'One single task';
+Route::get('/tasks', function () use($tasks) {
+  return view('index', [
+    'tasks' => $tasks
+  ]);
+})->name('tasks.index');
+
+Route::get('/tasks/{id}', function ($id) use($tasks) {
+  $task = collect($tasks)->firstWhere('id', $id);
+
+  if (!$task) {
+    abort(Response::HTTP_NOT_FOUND);
+  }
+
+  return view('show', ['task' => $task]);
 })->name('tasks.show');
 
 // Route::get('/hello', function() {
